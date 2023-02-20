a72b20466189b7b9a406c5c5ef2e4a23a95b92b2
Kieran Taylor <118790350+Kierantaylorsprojects@users.noreply.github.com>
2023-02-20 12:18:51 -0800

    Update Query #3.sql

--- EmployeeSQL/Query #3.sql
+++ EmployeeSQL/Query #3.sql
@@ -1,8 +1,8 @@
-SELECT titles.title, departments.dept_no, departments.dept_name, employees.emp_no, employees.last_name, employees.first_name
+SELECT titles.title, department.dept_no, department.dept_name, employees.emp_no, employees.last_name, employees.first_name
 FROM titles
 JOIN employees
-ON titles.title_id = employees.emp_title
-JOIN dept_manager
-ON employees.emp_no = dept_manager.emp_no
-JOIN departments
-ON dept_manager.dept_no = departments.dept_no
+ON titles.title_id = employees.emp_title_id
+JOIN dept_managers
+ON employees.emp_no = dept_managers.emp_no
+JOIN department
+ON dept_manager.dept_no = department.dept_no
