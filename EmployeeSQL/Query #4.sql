42115358598bfd54ce550e34e5b70744aab23ed7
Kieran Taylor <118790350+Kierantaylorsprojects@users.noreply.github.com>
2023-02-20 12:24:35 -0800

    Update Query #4.sql

--- EmployeeSQL/Query #4.sql
+++ EmployeeSQL/Query #4.sql
@@ -1,6 +1,6 @@
-SELECT dept_name.dept_no, employees.emp_no, employees.last_name, employees.first_name, department.dept_name
+SELECT dept_emp.dept_no, employees.emp_no, employees.last_name, employees.first_name, department.dept_emp
 FROM employees
-JOIN dept_name
+JOIN dept_emp
 ON employees.emp_no = dept_name.emp_no
 JOIN department
 ON dept_name.dept_no = department.dept_no
